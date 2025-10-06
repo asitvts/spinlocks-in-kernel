0f4a0b2e298a017987fab9413bfcebb87f4dabc4
asit vats <asitvts@gmail.com>
2025-10-06 14:47:42 +0530

    added multiple threads

--- spin.c
+++ spin.c
@@ -2,6 +2,12 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/kthread.h>	// for threads
+#include <linux/delay.h>	// for sleep
+
+
+#define NUM_THREADS 10
 
 
 MODULE_AUTHOR("asit");
@@ -12,22 +18,45 @@ MODULE_DESCRIPTION("spinlock code");
 static int glob=0;
 static spinlock_t my_spinlock;
 
-static void critical_section(void){
+static struct task_struct *threads[NUM_THREADS];	// threads pointer holder
 
+
+static void critical_section(void){	// critical section function
 	
 	unsigned long flags;
 	
-	
 	spin_lock_irqsave(&my_spinlock,flags);
 	
-	printk(KERN_INFO "inside critical section incrementing the global variable glob\n");
+	printk(KERN_INFO "critical section incrementing the global variable by thread %s\n", current->comm);
 	glob++;
 	printk(KERN_INFO "glob: %d\n",glob);
 	
 	spin_unlock_irqrestore(&my_spinlock,flags);
 	
+	
 	return;
 	
+}
+
+
+static int thread_func(void* data){
+
+	int id = *((int*)data);
+	kfree(data);
+	
+	printk(KERN_INFO "thread no %d is entering the critical section\n", id);
+	
+	while(!kthread_should_stop()){
+	
+		critical_section();
+		
+		msleep(1000);
+	
+	}
+	
+	
+	return 0;
+
 }
 
 
@@ -36,9 +65,23 @@ static int __init my_module(void){
 
 	spin_lock_init(&my_spinlock);
 	
-	critical_section();
-	critical_section();
-	critical_section();
+	
+	for(int i=0; i<NUM_THREADS; i++){
+		
+		int* id = kmalloc(1*sizeof(int), GFP_KERNEL);
+		if (!id) {
+		    printk(KERN_ERR "Failed to allocate memory for thread id %d\n", i);
+		    continue;
+		}
+		*id=i;
+		
+		threads[i] = kthread_run(thread_func, id, "my_thread_%d", i);
+		
+		if(IS_ERR(threads[i])){
+			printk(KERN_INFO "error making this thread\n");
+			threads[i]=NULL;
+		}
+ 	}
 	
 	
 	
@@ -48,7 +91,18 @@ static int __init my_module(void){
 
 static void __exit cleanup(void){
 	
-	printk(KERN_INFO "clean up done");
+	printk(KERN_INFO "cleaning up and stopping threads\n");
+	
+	for(int i=0; i<NUM_THREADS; i++){
+		
+		if(threads[i]){
+			kthread_stop(threads[i]);
+			threads[i]=NULL;
+		}
+		
+ 	}
+ 	
+	printk(KERN_INFO "clean up done\n");
 }
 
 
